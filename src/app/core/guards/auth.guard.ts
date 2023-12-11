1d985e9a46ab9d9840bd2eb578612d0c484c260c
hirkani <hirkanibhosale2001@gmail.com>
2023-12-11 15:28:15 +0530

    auth changes done

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+
+  const router = inject(Router)
+
+  const localStorageData = localStorage.getItem('loginDetails');
+  if(localStorageData){
+    return true;
+  }else{
+    router.navigateByUrl('login')
+  }return false;
+  
 };
